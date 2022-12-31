c6e1f8b31b9fb9ffd41fe92f692a921ff40d07dd
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2022-12-31 20:32:27 +0100

    ajout d'un aléatoire sur le message

--- Commandes/nulLucas.js
+++ Commandes/nulLucas.js
@@ -1,8 +1,29 @@
+const randomChoice = require("../Utils/Random/randomChoice");
+const list = [
+    "est nul",
+    "est un gros nul",
+    "sent la merde",
+    "sent le caca",
+    "est un gros caca",
+    "est mauvais",
+    "est mauvais en tout",
+    "est mauvais en tout et pour tout",
+    "est mauvais en tout et pour tout et pour rien",
+    "pue",
+    "pue la merde",
+    "pue le caca",
+    "pue la merde et le caca",
+    "pue fort",
+    "pue très fort"
+]
+
+randomChoice
+
 module.exports = {
     name: 'nul',
 
     async run(client, message, args) {
         let user = message.mentions.users.first() || message.author;
-        message.channel.send(`<@${user.id}> est nul !`);
+        await message.channel.send(`<@${user.id}> ${randomChoice(list)} !`);
     }
 }
