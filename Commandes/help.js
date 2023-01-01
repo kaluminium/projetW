7c8f0a1626008754ffdcec0139d5cc5047d76043
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-01-01 17:35:31 +0100

    creation de la commande help

---
+++ Commandes/help.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'help',
+    description: 'Affiche la liste des commandes',
+    category: 'Info',
+    usage: '[prefix]help',
+
+    async run(client, message, args) {
+        if (!args[0]) {
+            const embed = new Discord.EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('Liste des commandes')
+            .setDescription('Voici la liste des commandes disponibles')
+
+            const categories = Object.keys(client.categoryList);
+            for (let i = 0; i < categories.length; i++) {
+                const category = categories[i];
+                embed.addFields({name : category, value : client.categoryList[category].join(', ')});
+            }
+            return message.channel.send({embeds : [embed]});
+        }
+        else {
+            let command = client.commands.get(args[0]);
+            if(!command) return message.channel.send('Cette commande n\'existe pas');
+
+            const embed = new Discord.EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle(`Commande ${command.name}`)
+            .setDescription(command.description)
+            .addFields(
+                {name : 'Catégorie', value : command.category},
+                {name : 'Utilisation', value : command.usage}
+            )
+            return message.channel.send({embeds : [embed]});
+        }
+    }
+}
