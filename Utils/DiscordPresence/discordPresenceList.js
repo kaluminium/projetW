db0d514dfcea5b12f3099d9dc6eebaaa2d14a3c6
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-01-01 18:03:44 +0100

    liste des présences

---
+++ Utils/DiscordPresence/discordPresenceList.js
@@ -0,0 +1,18 @@
+const { ActivityType } = require("discord.js");
+
+const discordPresenceList = [
+    {
+        type : ActivityType.Watching,
+        name : 'gros caca de Lucas'
+    },
+    {
+        type : ActivityType.Playing,
+        name : 'joue avec moi UwU'
+    },
+    {
+        type : ActivityType.Watching,
+        name : 'Je pète partout'
+    }
+]
+
+module.exports = discordPresenceList;
