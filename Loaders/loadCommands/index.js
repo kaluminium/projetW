5e4394e0c1c7cbfe2137e16ec120b39b6fc8533d
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-01-01 17:37:00 +0100

    creation du fichier de chargement de commandes

---
+++ Loaders/loadCommands/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const loadCommandsVerification = require('./loadCommandsVerification.js');
+const loadCateogries = require('./loadCategories.js');
+
+module.exports = async client => {
+    client.categoryList = {};
+    fs.readdirSync('./Commandes/').filter(f => f.endsWith('.js')).forEach(async file => {
+        let command = require(`../../Commandes/${file}`);
+
+        if(!loadCommandsVerification(command, file)) return;
+        await loadCateogries(client, command);
+
+        client.commands.set(command.name, command);
+        client.commandsList.push(command.name);
+
+        console.log(`Commande chargée: ${command.name}`);
+    })
+}
