5687492c2240fe72f5e86001260ce6a11d77ab7a
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-01-01 18:03:29 +0100

    ajout de la presence du bot

--- Events/ready.js
+++ Events/ready.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
+const discordPresence = require('../Utils/DiscordPresence');
 
 module.exports = async client => {
     console.log(`Connecté en tant que ${client.user.tag}`);
+    discordPresence(client);
 }
